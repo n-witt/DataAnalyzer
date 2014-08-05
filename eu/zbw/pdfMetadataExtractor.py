c36d60d1feeb5a3d78ddbff42ad88050fa077ff3
Nils Witt <n.witt@zbw.eu>
2014-08-05 10:33:22 +0200

    some optimizations for the case that not metadata is available have been implemented. but also new errors have been introduced, so this commit should not be used. take the next.

--- eu/zbw/pdfMetadataExtractor.py
+++ eu/zbw/pdfMetadataExtractor.py
@@ -24,56 +24,88 @@ def main():
          metainfo = pdfFile.getDocumentInfo()
          #in case there are autor and title information in the metadata
          if metainfo.author != None or metainfo.title != None:
-            res = searchDataprovider(metainfo.author, metainfo.title)
+            res = searchDataprovider("search", "title", metainfo.title)
             printEconbizOutput(res)
          #when there is no metadata available
          else:
             hits = []
             s = pdf_to_txt(mypath + '/' + f, 0, 0)
-            paragraphs = re.split('\n+', s)
-            end = findThreshold(5, len(paragraphs))
+            paragraphs = re.split(' *\n+ *', s)
+            paragraphs = removeShortTerms(paragraphs, 5)
+            end = min(5, len(paragraphs))
             for a in range(0, end):
                #search only if there are more than min_tokens words
                if len(paragraphs[a].split()) > min_tokens:
                   res = searchDataprovider("search", "title", paragraphs[a])
-                  hit = selectBestMatch(paragraphs, res['hits']['hits'])
+                  hit = selectBestMatch(paragraphs, res['hits']['hits'], 50)
                   if hit != None:
                      bisect.insort_left(hits, hit)
             if len(hits) > 0:
                bestHit = hits[-1]
+               print "quality: " + str(bestHit[0])
+               #printEconbizOutput([bestHit[-1]])
+            else:
+               print "no hit"
+            print "\n"
       except (AttributeError, TypeError, PdfReadError, IOError, AssertionError) as e:
          continue
-   print(" done.")
+   print("done.")
 
+def removeShortTerms(l, length=5): 
+   l = uniqify(l)
+   removeCandidates = []
+   for s in l:
+      if len(s) < length:
+         removeCandidates.append(s)
+   for s in removeCandidates:
+      l.remove(s)
+   return l
+
+def uniqify(seq): 
+   seen = {}
+   result = []
+   for item in seq:
+      if item in seen: continue
+      seen[item] = 1
+      result.append(item)
+   return result
    
-def selectBestMatch(tokens, hits):
+def selectBestMatch(tokens, hits, qualityThreshold=50, creatorWeight=2, titleWeight=1):
    results = []
-   creatorWeight = 2
-   titleWeight = 1
    for hit in hits:
-      score = 0
       try:
+         persons = set()
+         normalizedScore = 0
+         creatorScore = 0
+         score = 0
          if hit.has_key('title'):
             foo = process.extractBests(hit['title'], tokens)
             if len(foo) > 0:
                score += foo[0][1] * titleWeight
          if hit.has_key('creator'):
-            creatorScore = 0
-            creators = hit['creator']
-            for creator in creators:
-               foo = process.extractBests(creator, tokens)
+            persons = persons.union(set(hit['creator']))
+         if hit.has_key('person'):
+            persons = persons.union(set(hit['person']))
+         if hit.has_key('contributor'):
+            persons = persons.union(set(hit['contributor']))
+            
+         if len(persons) > 0:
+            for person in persons:
+               foo = process.extractBests(person, tokens)
                if foo != None:
                   creatorScore += foo[0][1]
-            if len(creators) > 0:
-               score += (creatorScore/len(creators)) * creatorWeight
-         bisect.insort_left(results, (score/(creatorWeight+titleWeight), hit))
+            score += (creatorScore/len(persons)) * creatorWeight
+
+         normalizedScore = score/(creatorWeight+titleWeight)   
+         if normalizedScore >= qualityThreshold:
+            bisect.insort_left(results, (normalizedScore, hit))
       except UnicodeDecodeError as e:
          continue
    if len(results) > 0:
       return results[-1]
    return None
 
-def findThreshold(max, objectLen):
+def min(max, objectLen):
    if objectLen > max:
       end = max
    else:
